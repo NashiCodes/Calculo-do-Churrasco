1d8ca8a3fab5d9337ab5c336e4458c7ea6ad4aa6
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2022-08-04 15:39:53 -0300

    adiciona botão para remover

--- scripts/main.js
+++ scripts/main.js
@@ -8,17 +8,28 @@ let listP = document.querySelector("#listP");
 let table1 = document.querySelector("#table1");
 let table2 = document.querySelector("#table2");
 
+let removebtn = document.createElement("img");
+
+removebtn.id ="rBtn";
+removebtn.class ="remove-button";
+removebtn.src ="../images/x.png";
+
 let pessoas = 0;
 
 add.addEventListener("click", adiciona);
 custoF.addEventListener("click", Custos);
 perCap.addEventListener("click", Custos);
+removebtn.addEventListener("click", remove);
 
 function adiciona() {
   const p = document.createElement("li");
-  p.textContent = `${nome.value}`;
-  listP.appendChild(p);
-  pessoas++;
+  if(nome.value != ""){
+    p.textContent = `${nome.value}`;
+    listP.appendChild(p);
+    pessoas++;
+  }else {
+    window.alert("Digite um nome valido");
+  }
   nome.value = "";
 }
 
@@ -26,14 +37,25 @@ function Custos() {
   const custo = document.createElement("tr");
   const Produto = document.createElement("td");
   const val = document.createElement("td");
-  Produto.textContent = `${prod.value}`;
-  val.textContent = `${valor.value}`;
-  custo.appendChild(Produto);
-  custo.appendChild(val);
-
+  if(prod.value != ""){
+    Produto.textContent = `${prod.value}`;
+    custo.appendChild(Produto);
+  } else{
+    window.alert("Digite um produto valido");
+  }
+  if(valor.value != "" && valor.value > 0){
+    val.textContent = `${valor.value}`;
+    custo.appendChild(val);
+  } else{
+    window.alert("Digite um valor valido");
+  }
+  
   if (this.id == "CustoF") table1.appendChild(custo);
   else table2.appendChild(custo);
 
   prod.value = "";
   valor.value = "";
+}
+
+function remove (){
 }
